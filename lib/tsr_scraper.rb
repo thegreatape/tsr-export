07f3e1328721f313c1ef36f73839ec18b323a4f0
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-12-28 16:10:14 -0500

    Extract data from workouts, using retries to get around data sometimes being in the wrong format.

--- lib/tsr_scraper.rb
+++ lib/tsr_scraper.rb
@@ -9,13 +9,14 @@ class TSRScraper
     @username = username
     @password = password
     @start_date = start_date
+    @workout_data = []
 
     Capybara.register_driver :poltergeist do |app|
       Capybara::Poltergeist::Driver.new(app, js_errors: false)
     end
     Capybara.current_driver = :poltergeist
     Capybara.app_host = 'http://thesquatrack.com'
-    Capybara.default_wait_time = 20
+    Capybara.default_wait_time = 3
   end
 
   def log_in
@@ -29,6 +30,7 @@ class TSRScraper
   def export_all
     visit "/dashboard"
     export_workouts
+    puts @workout_data.flatten.join("\n")
   end
 
   private
@@ -42,8 +44,46 @@ class TSRScraper
   end
 
   def export_current_month
-    puts "processing #{current_date}"
-    puts workouts.map(&:text).join("\n")
+    $stderr.puts "processing #{current_date}"
+    workouts.each do |workout|
+      export_workout(workout)
+    end
+  end
+
+  def export_workout(workout, attempts = 0)
+    select_workout workout
+    select_export_format
+
+    export_data.tap do |data|
+      if !/^"Performed At"/.match(export_data)
+        if attempts < 20
+          return export_workout(workout, attempts+1)
+        else
+          save_and_open_screenshot
+          raise "Couldn't get correct workout export format, got #{data} instead"
+        end
+      end
+
+      @workout_data.push data.split("\n")
+    end
+  end
+
+  def export_data
+    find('.export_output textarea').value
+  end
+
+  def select_export_format
+    open_export_format_selection
+    find("ul.export_format_id li a", text: "CSV").click
+    sleep 1
+  end
+
+  def select_workout(workout)
+    workout.click
+  end
+
+  def open_export_format_selection
+    find("#foundWorkouts .dropdown-toggle").trigger(:click)
   end
 
   def workouts
